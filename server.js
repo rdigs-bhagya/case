faee05f7ede9597601621d51540d08b3d9140f74
bhagyashree patil <115216811+BHAGU2001@users.noreply.github.com>
2025-11-01 14:32:26 +0530

    server code added

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const http = require("http");
 const app = require("./index");
+
 const port = process.env.PORT || 5000;
+const host = "0.0.0.0"; // <-- Important for Render
 
 const server = http.createServer(app);
 
-server.listen(port, () => {
-  console.log("App is running on port " + port);
+server.listen(port, host, () => {
+  console.log(`🚀 App is running on http://${host}:${port}`);
 });
